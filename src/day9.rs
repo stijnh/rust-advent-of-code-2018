83ae5be164872fd191214d87341860178b8e5d98
Stijn <mail@stijnh.nl>
2018-12-11 23:43:59 +0100

    Slightly better solution for day 9

--- src/day9.rs
+++ src/day9.rs
@@ -1,42 +1,37 @@
+use std::collections::VecDeque;
+
+fn rotate<T>(vec: &mut VecDeque<T>, n: i32) {
+    if !vec.is_empty() {
+        if n > 0 {
+            for _ in 0..n {
+                let m = vec.pop_front().unwrap();
+                vec.push_back(m);
+            }
+        } else {
+            for _ in 0..-n {
+                let m = vec.pop_back().unwrap();
+                vec.push_front(m);
+            }
+        }
+    }
+}
+
 fn play_game(num_players: usize, num_marbles: usize) -> usize {
     let mut scores = vec![0; num_players];
-    let mut next = vec![!0; num_marbles];
-    let mut prev = vec![!0; num_marbles];
-    let mut cursor = 0;
+    let mut marbles = VecDeque::new();
 
-    prev[0] = 0;
-    next[0] = 0;
+    marbles.push_back(0);
 
     for i in 1..num_marbles {
         if i % 23 != 0 {
-            let a = next[cursor];
-            let b = next[a];
-
-            next[a] = i;
-            prev[i] = a;
-
-            prev[b] = i;
-            next[i] = b;
-
-            cursor = i;
+            rotate(&mut marbles, 2);
+            marbles.push_front(i);
         } else {
-            for _ in 0..7 {
-                cursor = prev[cursor];
-            }
+            rotate(&mut marbles, -7);
 
             let player = i % num_players;
             scores[player] += i;
-            scores[player] += cursor;
-
-            let (a, b) = (prev[cursor], next[cursor]);
-            next[a] = b;
-            prev[b] = a;
-
-            cursor = b;
-
-            if scores[player] == 8317 {
-                println!("{}", "!!!");
-            }
+            scores[player] += marbles.pop_front().unwrap();
         }
     }
 
