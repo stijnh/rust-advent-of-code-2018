71f2bddff09968cdcd0f070df7038a681390924d
stijnh <mail@stijnh.nl>
2018-12-12 10:32:50 +0100

    Completed day 12

---
+++ src/day12.rs
@@ -0,0 +1,62 @@
+use crate::common::{read_file_lines};
+use std::collections::HashMap;
+
+pub fn run(_: &[&str]) {
+    let lines = read_file_lines("inputs/day12");
+
+    // Load initial state
+    let header = "initial state: ";
+    let mut current = lines[0][header.len()..].chars().collect::<Vec<_>>();
+
+    // Load rules
+    let mut rules = HashMap::new();
+    for line in &lines[2..] {
+        let mut chars = line.chars().collect::<Vec<_>>();
+
+        let mut v = ['?'; 5];
+        v.copy_from_slice(&chars[..5]);
+        let a = chars[9];
+
+        rules.insert(v, a);
+    }
+
+    let mut first_plant = 0;
+    let mut scores = vec![];
+
+    // Simulate for 500 generations
+    for _ in 0..500usize {
+        let offset = current.iter().position(|x| *x == '.').unwrap_or(0);
+
+        let mut next = vec![];
+        next.extend(vec!['.'; 4]);
+        next.extend_from_slice(&current[offset..]);
+        next.extend(vec!['.'; 4]);
+        first_plant += offset as i32;
+
+        current = next
+            .windows(5)
+            .map(|m| rules[m])
+            .collect::<Vec<_>>();
+        first_plant -= 2;
+
+        let total = current
+            .iter()
+            .enumerate()
+            .filter(|(i, p)| **p == '#')
+            .map(|(i, p)| first_plant as i64 + i as i64)
+            .sum::<i64>();
+
+        scores.push(total);
+    }
+
+    // Question A: score at generation 20
+    println!("answer A: {}", scores[19]);
+
+    // Question A: score at generation 50000000000
+    // Assuming growth is linear, we just need to find increment per gen
+    let n = scores.len();
+    let incr = scores[n - 1] - scores[n - 2];
+    let cons = scores[n - 1] - incr * (n as i64 - 1);
+
+    println!("answer B: {}", cons + (50000000000i64 - 1) * incr);
+}
