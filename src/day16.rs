1171d30d30a765a60fad22918750417de1f0d49e
Stijn <mail@stijnh.nl>
2018-12-16 14:43:58 +0100

    Completed second part of day16.

--- src/day16.rs
+++ src/day16.rs
@@ -1,6 +1,8 @@
 use crate::common::read_file_lines;
 use regex::{Regex, Captures};
+use std::default::Default;
 
+type Instr = [i32; 4];
 type Regs = [i32; 4];
 
 fn parse_captures(re: &Regex, line: &str) -> [i32; 4] {
@@ -52,7 +54,16 @@ fn exec_instr(opcode: i32, a: i32, b: i32, c: i32, mut regs: Regs) -> Regs {
     regs
 }
 
-pub fn run(_: &[&str]) {
+fn name_instr(opcode: i32) -> &'static str {
+    let names = ["addr", "addi", "mulr", "muli",
+                 "banr", "bani", "borr", "bori",
+                 "setr", "seti", "gtir", "gtri",
+                 "gtrr", "eqir", "eqri", "eqrr"];
+
+    names.get(opcode as usize).unwrap_or(&"unknown")
+}
+
+fn parse_input() -> (Vec<(Regs, Instr, Regs)>, Vec<Instr>) {
     let before_re = Regex::new(r"Before: \[(\d+), (\d+), (\d+), (\d+)\]").unwrap();
     let instr_re = Regex::new(r"(\d+) (\d+) (\d+) (\d+)").unwrap();
     let after_re = Regex::new(r"After:  \[(\d+), (\d+), (\d+), (\d+)\]").unwrap();
@@ -79,9 +90,79 @@ pub fn run(_: &[&str]) {
 
         program.push(instr);
     }
+
+    (samples, program)
+}
+
+fn find_mapping(samples: &[(Regs, Instr, Regs)]) -> [i32; NUM_OPCODES] {
+    // we build a matrix in which element (i, j) indicates if the given
+    // opcode i could be mapping to the real opcode j. Eventually, this
+    // matrix should contain exactly one "true" on each row, indicating
+    // the given opcode can only be mapping to exactly one opcode.
+    let mut matrix = [[true; NUM_OPCODES]; NUM_OPCODES];
+    let mut mapping = [!0; NUM_OPCODES];
+
+    // build matrix.
+    for (before, instr, after) in samples.iter().cloned() {
+        let [old, a, b, c] = instr;
+
+        for new in 0..NUM_OPCODES {
+            let valid = exec_instr(new as i32, a, b, c, before) == after;
+
+            // Store if opcode "old" could be opcode "new"
+            matrix[old as usize][new] &= valid;
+        }
+    }
+
+    // check matrix.
+    for _ in 0..NUM_OPCODES {
+        let mut m = (!0, !0);
+
+        for i in 0..NUM_OPCODES {
+            print!("{} ", name_instr(i as i32));
+
+            for j in 0..NUM_OPCODES {
+                if matrix[j][i] || mapping[j] == i as i32 {
+                    print!("x ");
+                } else {
+                    print!(". ");
+                }
+            }
+
+            println!();
+        }
+
+        // find row with exactly one true element
+        for i in 0..NUM_OPCODES {
+            let count = matrix[i].iter().filter(|x| **x).count();
+            let pos = matrix[i].iter().position(|x| *x);
+
+            if count == 1 {
+                m = (i, pos.unwrap());
+            }
+        }
+
+        let (i, j) = m;
+        mapping[i] = j as i32;
+        println!(" mapping: {} = {}", name_instr(j as i32), i);
+        println!();
+
+        // clear row i and column j
+        for k in 0..NUM_OPCODES {
+            matrix[i][k] = false;
+            matrix[k][j] = false;
+        }
+    }
+
+    mapping
+}
+
+pub fn run(_: &[&str]) {
+    let (samples, program) = parse_input();
+    let mapping = find_mapping(&samples);
     
     let mut answer_a = 0;
-    for (before, instr, after) in samples.into_iter() {
+    for (before, instr, after) in samples.iter().cloned() {
         let [_, a, b, c] = instr;
         let count = (0..NUM_OPCODES)
             .filter(|op| exec_instr(*op as i32, a, b, c, before) == after)
@@ -94,5 +175,11 @@ pub fn run(_: &[&str]) {
 
     println!("answer A: {}", answer_a);
 
+    let mut regs = Default::default();
 
+    for [opcode, a, b, c] in program {
+        regs = exec_instr(mapping[opcode as usize] as i32, a, b, c, regs);
+    }
+
+    println!("answer B: {:?}", regs);
 }
