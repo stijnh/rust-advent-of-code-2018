817fa12e15c68671d3a458ff9e7645b78cf9f8c2
stijnh <mail@stijnh.nl>
2018-12-17 16:27:55 +0100

    Renamed dayX.rs to day0X.rs to correct sorting.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,15 @@ extern crate regex;
 
 #[macro_use]
 mod common;
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
 mod day12;
@@ -16,14 +24,6 @@ mod day13;
 mod day14;
 mod day16;
 mod day17;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 fn dummy(_: &[&str]) {
     println!("not implemented");
